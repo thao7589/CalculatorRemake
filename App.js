d78e44dee4aadefe23a739590316c89c8ca95f89
Thao Le <thao.le@s3corp.com.vn>
2019-11-19 17:06:18 +0700

    add UI

--- App.js
+++ App.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import InputNumberButton from './inputNumberButton';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.resultContainer}>
-        <Text></Text>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.initialState = {
+      result: '0',
+      operator: null,
+      firstValue: '',
+      secondValue: '',
+      nextValue: false
+    }
+    this.state = this.initialState;
+  }
+
+  renderButton = () => {
+    let layout = numbers.map((row, rowIndex) => {
+      let rowList = row.map((rowNumber, numberIndex) => {
+          return <InputNumberButton value={rowNumber} key={'btn-' + numberIndex} handleOnPress={this.onPress.bind(this, rowNumber)} />
+      })
+      return <View style={styles.rowInput} key={'row-' + rowIndex}>{rowList}</View>
+    })
+    return layout;
+  }
+
+  onPress = num => {
+    console.log(num)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.resultContainer}>
+          <Text style={styles.inputText}>{this.state.result}</Text>
+        </View>
+        <View style={styles.inputContainer}>
+            {this.renderButton()}
+        </View>
       </View>
-      <View style={styles.inputContainer}>
-        <Text></Text>
-      </View>
-    </View>
-  );
+    );
+  }
+  
 }
+
+const numbers = [
+  ['7', '8', '9', '/'],
+  ['4', '5', '6', 'x'],
+  ['1', '2', '3', '+'],
+  ['0', '.', '=', '-'],
+];
 
 const styles = StyleSheet.create({
   container: {
@@ -21,10 +59,23 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     flex: 2,
-    backgroundColor: 'grey'
+    backgroundColor: '#1E1240'
   },
   inputContainer: {
     flex: 8,
-    backgroundColor: 'blue'
-  }
+    justifyContent: 'center',
+    backgroundColor: '#3D0075'
+  },
+  inputText: {
+    color: '#fff',
+    fontSize: 80,
+    fontWeight: 'bold',
+    padding: 20,
+    textAlign: 'right'
+  },
+  rowInput: {
+    flex: 1,
+    flexDirection: 'row'
+  } 
+
 });
---
+++ inputNumberButton.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default class InputButton extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {value, handleOnPress} = this.props;
+        
+        return(
+            <TouchableOpacity style={styles.container} onPress={() => handleOnPress(value)}>
+                <Text style={styles.text}>{value}</Text>
+            </TouchableOpacity>    
+        )
+    }
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        margin: 1,
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        color: '#fff',
+        fontSize: 26
+    }
+})
